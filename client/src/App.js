c90961e360848de67d88571f359b5977b018b1d7
jesusre890 <jesusre890@gmail.com>
2023-07-07 20:46:17 -0300

    pi presentado

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./Views/Home/Home";
 import Landing from "./Views/Landing/Landing";
 import Detail from "./Views/Details/Detail";
 import Create from "./Views/Create/Create";
+import AboutMe from "./Views/AboutMe/AboutMe";
 
 function App() {
-
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Landing />} />
         <Route exact path="/home" element={<Home />} />
+        <Route path="/about" element={<AboutMe />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/create" element={<Create />} />
       </Routes>
